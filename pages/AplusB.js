4c97f79ccff07c5247abb15daec2273851cb30cd
larkenson <faustov.cfif@yandex.ru>
2023-09-09 15:53:56 +0700

    prob 122

--- pages/AplusB.js
+++ pages/AplusB.js
@@ -11,23 +11,30 @@ export default function Counter() {
 
 const [result, setresult] = useState(1);
 
-
- 
-
   async function conclusion1() {
     try {
       const response = await fetch("https://jsonplaceholder.typicode.com/todos/2");
     const resulte = await response.json();
     console.log("Успех:", JSON.stringify(resulte));
-     setresult(resulte);
+
+
+    function app(){
+      const resul = resulte.map(function(item) {
+        return <tr key={item.id}>
+          <td>{item.id}</td>
+          <td>{item.title}</td>
+        </tr>;
+    });
+
+
+    }
+     setresult({app});
     console.log(resulte);
   } 
   catch (error) {
       console.error("Ошибка:", error);
     }
-  
 }
-
 
   return (
     <SimpleGrid>
@@ -59,7 +66,6 @@ const [result, setresult] = useState(1);
 
   </Card>
   </SimpleGrid>
-
   )
 }
 
